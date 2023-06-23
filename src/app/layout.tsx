1f155616a17291a060dc9bec9cefc5eb382a4dfb
aletanus <alessandro@altadesign.com.br>
2023-06-23 11:31:13 -0300

    First commit.

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+import { Inter, Lexend } from "next/font/google";
+const lexend = Lexend({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-lexend",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata = {
+  title: "PokéVerse",
+  description: "O futuro Pokémon começa no PokéVerse",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body className= {`${lexend.variable} ${inter.className}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
