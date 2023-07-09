6e258ea1c674295fb3591daf334e38d52d0a010e
aletanus <alessandro@altadesign.com.br>
2023-07-09 01:37:35 -0300

    Fix: I have hidden a button that is currently not necessary.

--- src/components/SectionB.tsx
+++ src/components/SectionB.tsx
@@ -15,7 +15,7 @@ export default function SectionB() {
         <h1 className="font-black text-3xl mt-12 text-brand-0">PokéBusca</h1>
         <div className="flex h-[48px] w-[90%] justify-between mt-8">
           <SearchForm/>
-          <Button hiddenOnSmallScreens={true} onClick={getAllPokemon} type="button" style="searchAll" details="" size="medium">TODOS</Button>
+          {/* <Button hiddenOnSmallScreens={true} onClick={getAllPokemon} type="button" style="searchAll" details="" size="medium">TODOS</Button> */}
         </div>
         <div className="flex w-[90%] justify-between mt-8 320:h-[420px] 321:h-[460px]  370:h-[480px] 405:h-[530px] ">
           <CardsContainer/>
